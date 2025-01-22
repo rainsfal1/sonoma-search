28d510b6a2a5e72b91d141c7626fa59bd33d546e
rainsfall <59012139+rainsfal1@users.noreply.github.com>
2025-01-22 23:54:55 +0500

    Update metrics.rs to not reduplicate the metric initialization

--- services/crawler/src/metrics.rs
+++ services/crawler/src/metrics.rs
@@ -1,34 +1,46 @@
-use prometheus::{Counter, Histogram, IntGauge, register_int_gauge, register_counter, register_histogram};
+use prometheus::{Counter, Histogram, IntGauge, Opts, Registry};
 use lazy_static::lazy_static;
+use std::sync::Once;
+
+static METRICS_INIT: Once = Once::new();
 
 lazy_static! {
     // Gauges
-    pub static ref QUEUE_SIZE: IntGauge = register_int_gauge!(
-        "crawler_queue_size",
-        "Current number of URLs in the crawler queue"
+    pub static ref QUEUE_SIZE: IntGauge = IntGauge::with_opts(
+        Opts::new("crawler_queue_size", "Current number of URLs in the crawler queue")
     ).expect("Failed to create queue size gauge");
     
     // Counters
-    pub static ref PAGES_CRAWLED: Counter = register_counter!(
-        "crawler_pages_crawled",
-        "Total number of pages crawled"
+    pub static ref PAGES_CRAWLED: Counter = Counter::with_opts(
+        Opts::new("crawler_pages_crawled", "Total number of pages crawled")
     ).expect("Failed to create pages crawled counter");
     
-    pub static ref CRAWL_ERRORS: Counter = register_counter!(
-        "crawler_errors_total", 
-        "Total number of crawl errors"
+    pub static ref CRAWL_ERRORS: Counter = Counter::with_opts(
+        Opts::new("crawler_errors_total", "Total number of crawl errors")
     ).expect("Failed to create crawl errors counter");
     
-    pub static ref CRAWL_CYCLES: Counter = register_counter!(
-        "crawler_cycles_total",
-        "Total number of completed crawl cycles"
+    pub static ref CRAWL_CYCLES: Counter = Counter::with_opts(
+        Opts::new("crawler_cycles_total", "Total number of completed crawl cycles")
     ).expect("Failed to create crawl cycles counter");
     
     // Histograms
-    pub static ref CRAWL_DURATION: Histogram = register_histogram!(
-        "crawler_duration_seconds",
-        "Duration of crawl cycles in seconds"
+    pub static ref CRAWL_DURATION: Histogram = Histogram::with_opts(
+        Opts::new("crawler_duration_seconds", "Duration of crawl cycles in seconds")
     ).expect("Failed to create crawl duration histogram");
+}
+
+pub fn init_metrics() {
+    METRICS_INIT.call_once(|| {
+        // Only try to register metrics once
+        let registry = prometheus::default_registry();
+        
+        // Register metrics, ignoring AlreadyReg errors
+        let _ = registry.register(Box::new(QUEUE_SIZE.clone()));
+        let _ = registry.register(Box::new(PAGES_CRAWLED.clone()));
+        let _ = registry.register(Box::new(CRAWL_ERRORS.clone()));
+        let _ = registry.register(Box::new(CRAWL_CYCLES.clone()));
+        let _ = registry.register(Box::new(CRAWL_DURATION.clone()));
+    });
 }
 
 // Direct metric operations
